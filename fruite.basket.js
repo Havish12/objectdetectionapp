0c0e905d21028de667502754d83fedc89a20a87b
Havish12 <104434027+Havish12@users.noreply.github.com>
2023-05-04 16:57:53 +0800

    Add files via upload

---
+++ fruite.basket.js
@@ -0,0 +1,16 @@
+img="";
+anything="";
+
+function setup()
+{
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!");
+    objectDetector.detect(img, gotResult);
+}
